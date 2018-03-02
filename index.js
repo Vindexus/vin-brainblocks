ddc274bae27b6ec2466b182db69abca1a898ef7d
Vindexus <vindexus@gmail.com>
2018-03-01 21:56:03 -0800

    First strike

---
+++ index.js
@@ -0,0 +1,44 @@
+const request = require('superagent')
+
+function getToken (token, done) {
+  //Check with brainblocks to verify this token
+  const url = 'https://brainblocks.io/api/session/' + token + '/verify'
+  request.get(url)
+    .then((response) => {
+      console.log('response.body',response.body);
+      let bbData = JSON.parse(response.text)
+      return done(null, bbData)
+    })
+    .catch((err) => {
+      console.log('err',err);
+      done(err)
+    })
+
+}
+
+function confirmPayment (token, amount, currency, done) {
+  getToken(token, (err, result) => {
+    if (err) {
+      return done(err)
+    }
+
+    if (result.status == 'error') {
+      return done(new Error(result.message))
+    }
+
+    if (result.amount.toString() != amount.toString()) {
+      return done(new Error('BrainBlocks amount ' + result.amount + ' does not match order amount ' + amount))
+    }
+
+    if (result.currency.toString() != currency.toString()) {
+      return done(new Error('BrainBlocks currency ' + result.currency + ' does not match order currency ' + currency))
+    }
+
+    return done(null, true)
+  })
+}
+
+module.exports = {
+  getToken,
+  confirmPayment
+}
